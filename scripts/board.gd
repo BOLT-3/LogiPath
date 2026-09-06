a67b605d99f5aac3e7a6ef6120cdfe996f667676
fateless <tanmay.sahuff4141@gmail.com>
2026-09-06 12:51:21 +0530

    added tilemap child node and deleted the damned matrix

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,58 +1,48 @@
 extends Node2D
 
-const CELL_SIZE := 80.0
+const BOARD_SIZE := 8
 
-@onready var cells_visual = $CellsVisual
+@onready var tile_map: TileMapLayer = $TileMapLayer
 
 var cells: Dictionary = {}
 
-var board_shape = [
-	"11111",
-	"11111",
-	"11111",
-	"11111",
-	"11111"
-]
 
-
-func _ready():
+func _ready() -> void:
 	create_board()
-	cells_visual.set_board(self)
 
 
-func _unhandled_input(event):
-	if event is InputEventScreenTouch:
-		if event.pressed:
-			handle_press(event.position)
+func create_board() -> void:
+	tile_map.clear()
+	cells.clear()
 
-	elif event is InputEventMouseButton:
+	for y in range(BOARD_SIZE):
+		for x in range(BOARD_SIZE):
+			var position := Vector2i(x, y)
+
+			cells[position] = Cell.new(position)
+
+			# source_id = 0
+			# atlas_coords = (0, 0)
+			tile_map.set_cell(position, 0, Vector2i(0, 0))
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			handle_press(event.position)
 
+	elif event is InputEventScreenTouch:
+		if event.pressed:
+			handle_press(event.position)
 
-func handle_press(screen_position: Vector2):
-	var cell_position = screen_to_cell(screen_position)
+
+func handle_press(screen_position: Vector2) -> void:
+	var cell_position := screen_to_cell(screen_position)
 
 	if cells.has(cell_position):
 		print("Touched cell: ", cell_position)
 
 
 func screen_to_cell(screen_position: Vector2) -> Vector2i:
-	var local_position = to_local(screen_position)
-	
-	var x = floor(local_position.x / CELL_SIZE)
-	var y = floor(local_position.y / CELL_SIZE)
-	
-	return Vector2i(x, y)
-
-
-func create_board():
-	for y in range(board_shape.size()):
-		for x in range(board_shape[y].length()):
-			if board_shape[y][x] == "1":
-				add_cell(Vector2i(x, y))
-
-
-func add_cell(position: Vector2i):
-	var cell = Cell.new(position)
-	cells[position] = cell
+	var local_position := tile_map.to_local(screen_position)
+	return tile_map.local_to_map(local_position)
